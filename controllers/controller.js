e0d56b81cca99e871e03edd541768a437d2ff431
PascualHache <hector.pascual.diaz@gmail.com>
2020-02-18 11:27:19 +0100

    Implementación toggle

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,26 @@
 "use strict";
+window.onload = function initFunction() {
+    myToggle();
+};
 function createCar(plate, brand, color) {
     var car = new Car(plate, color, brand);
     car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerText = "CAR: PLATE: " + car.plate
+    var a = document.getElementById("carInfo");
+    a.innerText = "CAR: PLATE: " + car.plate
         + " COLOR: " + car.color + " BRAND: " + brand
         + " WHEELS: " + JSON.stringify(car.wheels);
+    ;
+    myToggle();
+}
+function myToggle() {
+    var x = document.getElementById("tab1");
+    var y = document.getElementById("tab2");
+    if (y.style.display === "none") {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+    else {
+        x.style.display = "block";
+        y.style.display = "none";
+    }
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,25 @@
-
-function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
-    document.body.innerText="CAR: PLATE: " + car.plate 
-    + " COLOR: " +car.color + " BRAND: " + brand 
-    + " WHEELS: " + JSON.stringify(car.wheels);
+window.onload = function initFunction() {
+  myToggle();
 }
 
+function createCar(plate: string, brand: string, color: string) {
+  let car = new Car(plate, color, brand);
+  car.addWheel(new Wheel(2, "SEAT"));
+  var a: any=document.getElementById("carInfo");
+  a.innerText = "CAR: PLATE: " + car.plate
+  + " COLOR: " + car.color + " BRAND: " + brand
+  + " WHEELS: " + JSON.stringify(car.wheels);;
+    myToggle();
+}
+
+function myToggle() {
+  var x: any = document.getElementById("tab1");
+  var y: any = document.getElementById("tab2");
+  if (y.style.display === "none") {
+    x.style.display = "none";
+    y.style.display = "block";
+  } else {
+    x.style.display = "block";
+    y.style.display = "none";
+  }
+}
