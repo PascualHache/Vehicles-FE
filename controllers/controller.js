c62ce079ae3fd5961ed4686e1ded904d79b06899
PascualHache <hector.pascual.diaz@gmail.com>
2020-02-18 13:51:40 +0100

    Button problem solved

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,22 +5,39 @@ window.onload = function initFunction() {
 function createCar(plate, brand, color) {
     var car = new Car(plate, color, brand);
     car.addWheel(new Wheel(2, "SEAT"));
-    var a = document.getElementById("carInfo");
-    a.innerText = "CAR: PLATE: " + car.plate
-        + " COLOR: " + car.color + " BRAND: " + brand
-        + " WHEELS: " + JSON.stringify(car.wheels);
-    ;
+    addRowCarTable(car.plate, car.color, JSON.stringify(car.wheels));
+    // var a: any = document.getElementById("carInfo");
+    // a.innerText = "CAR: PLATE: " + car.plate
+    //   + " COLOR: " + car.color + " BRAND: " + brand
+    //   + " WHEELS: " + JSON.stringify(car.wheels);
     myToggle();
+    var z = document.getElementById("tablita");
+    z.style.visibility = "visible";
+}
+function myTest() {
+    createCar('1212SDS', 'SEAT', 'ROJO');
 }
 function myToggle() {
     var x = document.getElementById("tab1");
     var y = document.getElementById("tab2");
+    var z = document.getElementById("tablita");
     if (y.style.display === "none") {
         x.style.display = "none";
         y.style.display = "block";
+        z.style.visibility = "visible";
     }
     else {
         x.style.display = "block";
         y.style.display = "none";
     }
+}
+function addRowCarTable(plate, brand, color) {
+    var table = document.getElementById("tablita");
+    var row = table.insertRow(2);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = plate;
+    cell2.innerHTML = brand;
+    cell3.innerHTML = color;
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -5,21 +5,45 @@ window.onload = function initFunction() {
 function createCar(plate: string, brand: string, color: string) {
   let car = new Car(plate, color, brand);
   car.addWheel(new Wheel(2, "SEAT"));
-  var a: any=document.getElementById("carInfo");
-  a.innerText = "CAR: PLATE: " + car.plate
-  + " COLOR: " + car.color + " BRAND: " + brand
-  + " WHEELS: " + JSON.stringify(car.wheels);;
-    myToggle();
+
+  addRowCarTable(car.plate, car.color, JSON.stringify(car.wheels));
+
+  // var a: any = document.getElementById("carInfo");
+  // a.innerText = "CAR: PLATE: " + car.plate
+  //   + " COLOR: " + car.color + " BRAND: " + brand
+  //   + " WHEELS: " + JSON.stringify(car.wheels);
+
+  myToggle();
+  var z: any = document.getElementById("tablita");
+  z.style.visibility = "visible";
+}
+
+function myTest(){
+  createCar('1212SDS','SEAT','ROJO');
 }
 
 function myToggle() {
   var x: any = document.getElementById("tab1");
   var y: any = document.getElementById("tab2");
+  var z: any = document.getElementById("tablita");
   if (y.style.display === "none") {
     x.style.display = "none";
     y.style.display = "block";
+    z.style.visibility = "visible";
   } else {
     x.style.display = "block";
     y.style.display = "none";
+
   }
+}
+
+function addRowCarTable(plate:string,brand:string,color:string) {
+  var table:any = document.getElementById("tablita");
+  var row = table.insertRow(2);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = plate;
+  cell2.innerHTML = brand;
+  cell3.innerHTML = color;
 }
