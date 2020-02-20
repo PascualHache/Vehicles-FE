c4f845c3f3593b06213f104be86f297798052669
PascualHache <hector.pascual.diaz@gmail.com>
2020-02-20 13:59:53 +0100

    Fase dos implementada, pendiente fase3

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,40 +4,105 @@ window.onload = function initFunction() {
 };
 function createCar(plate, brand, color) {
     var car = new Car(plate, color, brand);
-    car.addWheel(new Wheel(2, "SEAT"));
-    addRowCarTable(car.plate, car.color, JSON.stringify(car.wheels));
-    // var a: any = document.getElementById("carInfo");
-    // a.innerText = "CAR: PLATE: " + car.plate
-    //   + " COLOR: " + car.color + " BRAND: " + brand
-    //   + " WHEELS: " + JSON.stringify(car.wheels);
+    for (var index = 0; index < 4; index++) {
+        car.addWheel(new Wheel((index + 1), "Unknown"));
+    }
+    var carList = [car.plate, car.brand, car.color];
+    addRowCarTable(carList);
+    // JSON.stringify(car.wheels)
     myToggle();
-    var z = document.getElementById("tablita");
-    z.style.visibility = "visible";
 }
-function myTest() {
-    createCar('1212SDS', 'SEAT', 'ROJO');
+function fillCar() {
+    var plate = document.getElementById("plate");
+    var brand = document.getElementById("brand");
+    var color = document.getElementById("color");
+    // var car = new Car(plate, brand, color);
+    // for (let index = 0; index < 4; index++) {
+    //   car.addWheel(new Wheel((index + 1), "Unknown"));
+    // }
+    var carList = [plate.value, brand.value, color.value];
+    addRowCarTable(carList);
+    // JSON.stringify(car.wheels)
+    myToggle();
+    return carList;
+}
+function updateWheels() {
+    var objWheels = [];
+    var table = document.getElementById("tablita2");
+    var row = table.insertRow(2);
+    for (var index = 0; index < 4; index++) {
+        var cell = row.insertCell(index);
+        var w = document.getElementById("WheelBrand" + [index + 1]);
+        var d = document.getElementById('WheelD' + [index + 1]);
+        cell.innerHTML = w.value + " : " + d.value;
+    }
 }
 function myToggle() {
     var x = document.getElementById("tab1");
     var y = document.getElementById("tab2");
-    var z = document.getElementById("tablita");
+    var t1 = document.getElementById("tablita1");
+    var t2 = document.getElementById("tablita2");
+    var b1 = document.getElementById("button1");
+    var b2 = document.getElementById("button2");
     if (y.style.display === "none") {
         x.style.display = "none";
         y.style.display = "block";
-        z.style.visibility = "visible";
+        t1.style.visibility = "visible";
+        t2.style.visibility = "visible";
+        b1.style.display = "none";
+        b2.style.display = "block";
     }
     else {
         x.style.display = "block";
         y.style.display = "none";
+        b1.style.display = "block";
+        b2.style.display = "none";
+        t2.style.visibility = "hidden";
     }
 }
-function addRowCarTable(plate, brand, color) {
-    var table = document.getElementById("tablita");
+function addRowCarTable(carList) {
+    var table = document.getElementById("tablita1");
     var row = table.insertRow(2);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = plate;
-    cell2.innerHTML = brand;
-    cell3.innerHTML = color;
+    for (var index = 0; index < 3; index++) {
+        var cell = row.insertCell(index);
+        cell.innerHTML = carList[index];
+    }
+}
+function validateCarFields() {
+    var plateTest = document.getElementById("plate");
+    var BrandTest = document.getElementById("brand");
+    var ColorTest = document.getElementById("color");
+    if (plateTest.checkValidity()) {
+        console.log("Esta bien!");
+        if (BrandTest.validity.valueMissing || ColorTest.validity.valueMissing) {
+            alert("Todos los campos deben ser completados");
+        }
+        else {
+            fillCar();
+        }
+    }
+    else {
+        if (plateTest.validity.valueMissing) {
+            alert("El campo matrícula está vacío. introduce una matrícula válida");
+        }
+        else if (plateTest.validity.patternMismatch) {
+            alert("La matricula debe contener 4 digitos y 3 letras");
+        }
+    }
+}
+function validateWheelDiam() {
+    var isError = false;
+    for (var index = 0; index < 4; index++) {
+        // let w: any = document.getElementById("WheelBrand"+[index+1]);
+        var d = document.getElementById('WheelD' + [index + 1]);
+        var num = d.value;
+        if (!(num > 0.4 && num < 2)) {
+            alert("El diámetro de la rueda " + (index + 1) + " no es correcto");
+            isError = true;
+            break;
+        }
+    }
+    if (!isError) {
+        updateWheels();
+    }
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -3,47 +3,111 @@ window.onload = function initFunction() {
 }
 
 function createCar(plate: string, brand: string, color: string) {
-  let car = new Car(plate, color, brand);
-  car.addWheel(new Wheel(2, "SEAT"));
+  var car = new Car(plate, color, brand);
 
-  addRowCarTable(car.plate, car.color, JSON.stringify(car.wheels));
-
-  // var a: any = document.getElementById("carInfo");
-  // a.innerText = "CAR: PLATE: " + car.plate
-  //   + " COLOR: " + car.color + " BRAND: " + brand
-  //   + " WHEELS: " + JSON.stringify(car.wheels);
-
+  for (let index = 0; index < 4; index++) {
+    car.addWheel(new Wheel((index + 1), "Unknown"));
+  }
+  var carList: any = [car.plate, car.brand, car.color]
+  addRowCarTable(carList);
+  // JSON.stringify(car.wheels)
   myToggle();
-  var z: any = document.getElementById("tablita");
-  z.style.visibility = "visible";
 }
 
-function myTest(){
-  createCar('1212SDS','SEAT','ROJO');
+function fillCar() {
+  let plate: any = document.getElementById("plate");
+  let brand: any = document.getElementById("brand");
+  let color: any = document.getElementById("color");
+  // var car = new Car(plate, brand, color);
+  // for (let index = 0; index < 4; index++) {
+  //   car.addWheel(new Wheel((index + 1), "Unknown"));
+  // }
+  var carList: any = [plate.value, brand.value, color.value]
+  addRowCarTable(carList);
+  // JSON.stringify(car.wheels)
+  myToggle();
+  return carList;
+}
+
+function updateWheels() {
+  var objWheels: any = [];
+  let table: any = document.getElementById("tablita2");
+  let row = table.insertRow(2);
+  for (let index = 0; index < 4; index++) {
+    let cell = row.insertCell(index);
+    let w: any = document.getElementById("WheelBrand" + [index + 1]);
+    let d: any = document.getElementById('WheelD' + [index + 1]);
+    cell.innerHTML = `${w.value} : ${d.value}`;
+  }
 }
 
 function myToggle() {
-  var x: any = document.getElementById("tab1");
-  var y: any = document.getElementById("tab2");
-  var z: any = document.getElementById("tablita");
+  let x: any = document.getElementById("tab1");
+  let y: any = document.getElementById("tab2");
+  let t1: any = document.getElementById("tablita1");
+  let t2: any = document.getElementById("tablita2");
+  let b1: any = document.getElementById("button1");
+  let b2: any = document.getElementById("button2");
   if (y.style.display === "none") {
     x.style.display = "none";
     y.style.display = "block";
-    z.style.visibility = "visible";
+    t1.style.visibility = "visible";
+    t2.style.visibility = "visible";
+    b1.style.display = "none";
+    b2.style.display = "block"
   } else {
     x.style.display = "block";
     y.style.display = "none";
+    b1.style.display = "block";
+    b2.style.display = "none"
+    t2.style.visibility = "hidden";
+
 
   }
 }
 
-function addRowCarTable(plate:string,brand:string,color:string) {
-  var table:any = document.getElementById("tablita");
-  var row = table.insertRow(2);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  cell1.innerHTML = plate;
-  cell2.innerHTML = brand;
-  cell3.innerHTML = color;
+function addRowCarTable(carList: any) {
+  let table: any = document.getElementById("tablita1");
+  let row = table.insertRow(2);
+  for (let index = 0; index < 3; index++) {
+    let cell = row.insertCell(index);
+    cell.innerHTML = carList[index];
+  }
+}
+
+function validateCarFields() {
+  let plateTest: any = document.getElementById("plate");
+  let BrandTest: any = document.getElementById("brand");
+  let ColorTest: any = document.getElementById("color");
+  if (plateTest.checkValidity()) {
+    console.log("Esta bien!");
+    if (BrandTest.validity.valueMissing || ColorTest.validity.valueMissing) {
+      alert("Todos los campos deben ser completados")
+    } else {
+      fillCar()
+    }
+  } else {
+    if (plateTest.validity.valueMissing) {
+      alert("El campo matrícula está vacío. introduce una matrícula válida")
+    } else if (plateTest.validity.patternMismatch) {
+      alert("La matricula debe contener 4 digitos y 3 letras")
+    }
+  }
+}
+
+function validateWheelDiam() {
+  let isError: boolean = false;
+  for (let index = 0; index < 4; index++) {
+    // let w: any = document.getElementById("WheelBrand"+[index+1]);
+    let d: any = document.getElementById('WheelD' + [index + 1]);
+    var num: number = d.value;
+    if (!(num > 0.4 && num < 2)) {
+      alert("El diámetro de la rueda " + (index + 1) + " no es correcto")
+      isError = true;
+      break;
+    }
+  }
+  if (!isError) {
+    updateWheels();
+  }
 }
